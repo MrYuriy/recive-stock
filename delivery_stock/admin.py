2b13c5f2b9617024c641026a301aed32de02700f
MrYuriy <yuriybabiyk@gmail.com>
2024-10-21 08:01:32 +0200

    Fix loading Container line

--- delivery_stock/admin.py
+++ delivery_stock/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
 from .models import (
-    ReasoneComment, Location, 
-    SuplierSKU, Supplier, SecondRecDelivery,
-    ImageModel, FirstRecDelivery,
-    DeliveryContainer, ContainerLine
-    )
+    ReasoneComment,
+    Location,
+    SuplierSKU,
+    Supplier,
+    SecondRecDelivery,
+    ImageModel,
+    FirstRecDelivery,
+    DeliveryContainer,
+    ContainerLine,
+)
 
 
 class SupplierAdmin(admin.ModelAdmin):
@@ -18,10 +23,28 @@ class FirstRecDeliveryAdmin(admin.ModelAdmin):
     search_fields = ("identifier",)
     list_filter = ("identifier",)
 
+
 class SecondRecDeliveryAdmin(admin.ModelAdmin):
     list_display = ("identifier", "pre_advice_nr", "master_nr")
     search_fields = ("identifier",)
     list_filter = ("identifier",)
+
+
+class ContainerLineAdmin(admin.ModelAdmin):
+    list_display = (
+        "line_nr",
+        "container",
+        "suplier_sku",
+        "not_sys_barcode",
+        "recive_unit",
+        "qty_unit",
+    )
+    search_fields = ("not_sys_barcode", "suplier_sku__sku", "container__identifier")
+    list_filter = ("recive_unit", "container")
+
+    raw_id_fields = ("suplier_sku",)
+
+    filter_horizontal = ("images_url",)
 
 
 admin.site.register(Location)
@@ -32,4 +55,4 @@ admin.site.register(ReasoneComment)
 admin.site.register(ImageModel)
 admin.site.register(SecondRecDelivery, SecondRecDeliveryAdmin)
 admin.site.register(DeliveryContainer)
-admin.site.register(ContainerLine)
+admin.site.register(ContainerLine, ContainerLineAdmin)
